db55537449deed683c48d991905c6a7fc744ff21
Deven Phillips <deven.phillips@gmail.com>
2017-02-13 13:42:49 -0500

    Cleaned up some expression language code

--- src/main/java/us/juggl/twentyseventeen/february/MyRouteBuilder.java
+++ src/main/java/us/juggl/twentyseventeen/february/MyRouteBuilder.java
@@ -10,16 +10,17 @@ import org.apache.camel.spi.Registry;
 public class MyRouteBuilder extends RouteBuilder {
 
     public static final String SEDA_OPTS = "?multipleConsumers=true&concurrentConsumers=5";
+    static final String QUERY = "INSERT INTO tweets (id, status, created, screenname) VALUES (:?id, :?status, :?created, :?screenname)";
 
     /**
      * Let's configure the Camel routing rules using Java code...
      */
     public void configure() {
+        Registry reg = getContext().getRegistry();
 
         StringBuilder uriBuilder = new StringBuilder("twitter://streaming/filter?");
         uriBuilder.append("type=event&");
         uriBuilder.append("lang=en&");
-        Registry reg = getContext().getRegistry();
         uriBuilder.append("consumerKey=").append(reg.lookupByName("api.key")).append("&");
         uriBuilder.append("consumerSecret=").append(reg.lookupByName("api.secret")).append("&");
         uriBuilder.append("accessToken=").append(reg.lookupByName("access.token")).append("&");
@@ -29,16 +30,16 @@ public class MyRouteBuilder extends RouteBuilder {
         //String keywords = "#HogwartsRunningClub,#GeeksForGood,#HRCStrong,#HRCHustlePuff17,#HRCRacenclaw17,#HRCSlytheRun17,#HRCGryffinDash17,#HRCFacultyBeasts17,#somuchgood";
         String keywords = "#grammys";
 
-        ValueBuilder query = constant("INSERT INTO tweets (id, status, created, screenname) VALUES (:?id, :?status, :?created, :?screenname)");
+        ValueBuilder queryConst = constant(QUERY);
         from("seda:tweet2db" + SEDA_OPTS + "&size=" + reg.lookupByName("db.pool"))
-                .setHeader("id", simple("${body.getId()}"))
-                .setHeader("status", simple("${body.getText()}"))
-                .setHeader("created", simple("${body.getCreatedAt().getTime()}"))
-                .setHeader("screenname", simple("${body.getUser().getScreenName()}"))
-                .setBody(query)
-                .to("jdbc:lykely?useHeadersAsParameters=true");
+            .setHeader("id", simple("${body.id}"))                      // Parse the tweet properties
+            .setHeader("status", simple("${body.text}"))                // into SQL values
+            .setHeader("created", simple("${body.createdAt.time}"))     // stored in message headers
+            .setHeader("screenname", simple("${body.user.screenName}")) // on the Camel Exchange
+            .setBody(queryConst)                                 // Set the prepared statement query
+            .to("jdbc:lykely?useHeadersAsParameters=true"); // Execute query using parameters from header values
 
-        from("seda:tweet2log" + SEDA_OPTS).sample(10).log(simple("${body.getText()}").getText());
+        from("seda:tweet2log" + SEDA_OPTS).sample(10).log("${body.text}");
 
         from(baseUri+"&keywords="+keywords)
             .filter(simple("${body.isRetweet()} == false"))
